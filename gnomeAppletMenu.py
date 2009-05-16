1d8a48c555527a0f061c060149102c79ee8d6bd2
Super Snot <snot@mother.(none)>
2009-05-16 12:40:53 +0200

    Deleted and cleaned the code

--- gnomeAppletMenu.py
+++ gnomeAppletMenu.py
@@ -4,14 +4,13 @@ import pygtk
 import sys
 pygtk.require('2.0')
 
+import gnomeapplet
+import gtk
+
 from xml.dom import minidom#, Node
 
-def xml():
-	doc = minidom.parse("menu.xml")
-	rootNode = doc.documentElement
-	rec_xml(rootNode)
 
-def rec_xml(node):
+def create_menu(node):
 	for child in node.childNodes:
 		menu = gtk.Menu()
 		if child.localName == "item":
@@ -20,14 +19,41 @@ def rec_xml(node):
 			menu.append(gtk.SeparatorMenuItem())
 		if child.localName == "menu": # && child.childNodes: #isnt the && redundant since a menu always have childnodes?
 			parent = gtk.MenuItem(child.getAttribute("name"))
-			parent.set_submenu(rec_xml(child))
+			parent.set_submenu(create_menu(child))
 	return menu
 
 
-import gnomeapplet
-import gtk
+def factory(applet, iid):
+	menu_bar = gtk.MenuBar()
+	doc = minidom.parse("menu.xml")
+	rootNode = doc.documentElement
 
-def create_menu():
+	menu = create_menu(rootNode)
+	menu_bar.append(menu)
+
+	applet.add(menu_bar)
+	applet.show_all()
+	return True
+
+
+if len(sys.argv) == 2:
+	if sys.argv[1] == "run-in-window":
+		mainWindow = gtk.Window(gtk.WINDOW_TOPLEVEL)
+		mainWindow.set_title("Gnome Applet Menu")
+		mainWindow.connect("destroy", gtk.main_quit)
+		applet = gnomeapplet.Applet()
+		factory(applet, None)
+		applet.reparent(mainWindow)
+		mainWindow.show_all()
+		gtk.main()
+		sys.exit()
+
+if __name__ == '__main__':
+	print "Starting factory"
+	gnomeapplet.bonobo_factory("OAFIID:Gnome_Panel_Menu_Factory", gnomeapplet.Applet.__gtype__, "gnome applet menu", "0.1", factory)
+
+
+def create_menu_old():
 	menu = gtk.Menu()
 
 	menu.append(gtk.MenuItem("Open"))
@@ -57,33 +83,4 @@ def create_menu():
 	menu_bar.append(rec_xml(rootNode))
 
 	return menu_bar
-
-
-
-def factory(applet, iid):
-#	xml()
-#	label = gtk.Label("Success!")
-#	applet.add(label)
-
-	applet.add(create_menu())
-
-	applet.show_all()
-	return True
-
-
-if len(sys.argv) == 2:
-	if sys.argv[1] == "run-in-window":
-		mainWindow = gtk.Window(gtk.WINDOW_TOPLEVEL)
-		mainWindow.set_title("Gnome Applet Menu")
-		mainWindow.connect("destroy", gtk.main_quit)
-		applet = gnomeapplet.Applet()
-		factory(applet, None)
-		applet.reparent(mainWindow)
-		mainWindow.show_all()
-		gtk.main()
-		sys.exit()
-
-if __name__ == '__main__':
-	print "Starting factory"
-	gnomeapplet.bonobo_factory("OAFIID:Gnome_Panel_Menu_Factory", gnomeapplet.Applet.__gtype__, "gnome applet menu", "0.1", factory)
 
